e4032acc12d9775b862af423ad9b4e64afa4c2a0
furyjack <lakshaytaneja26@gmail.com>
2017-01-13 21:07:50 +0530

    ADDED REPOCONTRACT

---
+++ app/src/main/java/play/android/com/trackthehub/data/MyContract.java
@@ -0,0 +1,32 @@
+package play.android.com.trackthehub.data;
+
+
+import android.provider.BaseColumns;
+
+public class MyContract {
+
+
+    public static final class RepoEntry implements BaseColumns
+    {
+       public static final String TABLE_NAME="Myrepos";
+        public static final String COLUMN_TITLE="TITLE";
+        public static final String COLUMN_DESC="DESC";
+        public static final String COLUMN_LANG="LANG";
+        public static final String COLUMN_STARS="STARS";
+        public static final String COLUMN_FORKS="FORKS";
+        public static final String COLUMN_STODAY="STODAY";
+
+        
+
+
+
+
+
+
+
+
+    }
+
+
+
+}
