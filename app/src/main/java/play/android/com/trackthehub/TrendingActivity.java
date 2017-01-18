5066232cfdb90d14a028dc17c79f52e58ce423e5
furyjack <lakshaytaneja26@gmail.com>
2017-01-18 12:23:40 +0530

    some activities and layouts added

---
+++ app/src/main/java/play/android/com/trackthehub/TrendingActivity.java
@@ -0,0 +1,13 @@
+package play.android.com.trackthehub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TrendingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trending);
+    }
+}
